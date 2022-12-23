af9fc919339b7ef3d502343e0e452600d2235428
bfearon <115654051+bfearon@users.noreply.github.com>
2022-12-22 19:51:58 -0800

    second commit

--- contactus.php
+++ contactus.php
@@ -25,21 +25,21 @@
   <tbody>
     <tr>
      
-    <td>animecentral@gmail.com</td>
+    <td>IT_Conference2022@gmail.com</td>
 
       <td>Facebook</td>
-      <td> <a href="https://www.facebook.com/">@animecentral</a> </td>
+      <td> <a href="https://www.facebook.com/">IT Conference 2022</a> </td>
     </tr>
     <tr>
      
       <td></td>
       <td>Twitter</td>
-      <td><a href="https://twitter.com/">@anime_central </a></td>
+      <td><a href="https://twitter.com/">IT Conference 2022 </a></td>
     </tr>
    
     <td></td>
       <td>Instagram</td>
-      <td> <a href="https://www.instagram.com/">@animecentral1</a></td>
+      <td> <a href="https://www.instagram.com/">IT Conference 2022</a></td>
     </tr>
 
   </tbody>
@@ -47,11 +47,7 @@
 
 <br>
 <br>
-<div class="mapouter"><div class="gmap_canvas"><iframe class="gmap_iframe" width="100%" frameborder="0" scrolling="no" marginheight="0" marginwidth="0" 
-src="https://maps.google.com/maps?width=800&amp;height=400&amp;hl=en&amp;q=jamaica&amp;t=h&amp;z=14&amp;ie=UTF8&amp;iwloc=B&amp;output=embed"></iframe>
-<a href="https://piratebay-proxys.com/">Piratebay</a></div><style>.mapouter{position:relative;text-align:right;width:100%;height:400px;}.gmap_canvas 
-{overflow:hidden;background:none!important;width:100%;height:400px;}.gmap_iframe {height:400px!important;}</style></div>
-    </div>
+
 
 
 
